0b598f152e4f8b98f406d980972ee7846935f7f5
Caio Lesnock <caio.dev@outlook.com>
2023-08-18 22:29:14 -0300

    Gui structure

--- game.js
+++ game.js
@@ -1,11 +1,29 @@
 import { drawLines, drawX, drawCircle } from './renderer.js'
+import { getSquareNumber } from './utils.js'
 const canvas = document.querySelector('canvas')
 
 const size = 300
 canvas.width = size
 canvas.height = size
+let turn = 'x'
 
 drawLines()
+setMouseListener()
 
-drawX(8)
-drawCircle(7)
+function setMouseListener() {
+  canvas.addEventListener('click', event => {
+    const bounds = canvas.getBoundingClientRect()
+    const squareSize = bounds.width / 3
+    const column = Math.floor(event.offsetX / squareSize)
+    const row = Math.floor(event.offsetY / squareSize)
+    const squareNumber = getSquareNumber(row, column)
+
+    if (turn == 'x') {
+      drawX(squareNumber)
+    } else {
+      drawCircle(squareNumber)
+    }
+
+    turn = turn == 'x' ? 'o' : 'x'
+  })
+}
